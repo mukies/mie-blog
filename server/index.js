9e29c10e548e494c6ae1151443fb8b107d72b44d
mukies <muk.yess@gmai.com>
2024-04-14 20:08:29 +0545

    deployment

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const postRoutes = require("./routes/post.route");
 const messageRoutes = require("./routes/message.route");
 const adminRoutes = require("./routes/admin.route");
 const { app, server } = require("./socket/socket");
+const path = require("path");
+const port = process.env.PORT;
+// let __dirname = path.resolve();
 
 // database
 require("./db/config");
 
 // middlewares
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
 
 // cloudinary
@@ -28,7 +32,18 @@ app.use("/api/post", postRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/admin", adminRoutes);
 
-const port = process.env.PORT;
+// http://localhost:8080 => backend,frontend
+
+if (process.env.NODE_ENV == "prod") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  // react app
+  app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+  });
+}
+
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
